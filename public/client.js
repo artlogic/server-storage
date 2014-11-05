79ac0d2727052635366ac2c3d2f584960d5c835e
James Kruth <james@arsmentis.com>
2014-11-05 11:40:09 -0500

    Untested, but hopefully working client.js

--- public/client.js
+++ public/client.js
@@ -1,24 +1,72 @@
-var serverStorage = (function($) {
-    var pub = {};
+var serverStorage = (function (global, $) {
+    "use strict";
 
-    // TODO: figure out the proper api key
+    // we could potentially sense the base host
+    var base = '//nsbapp.com/storage',
+        pub = {},
+        // find the first part of a path, for example: given '/HelloWorld/index.html
+        // return /HelloWorld/ and HelloWorld (first group)
+        ns_re = new RegExp('/([^/]*)/'),
+        // if there's a problem determining the namespace, set a default one
+        ns_raw = ns_re.exec(global.document.location.pathname),
+        namespace = ns_raw ? ns_raw[1] : 'default';
 
-    pub.clear = function() {
+    // both url and data are optional
+    function makeRequest(method, url, data) {
+        if ($.type(url) === 'object') {
+            data = url;
+            url = '';
+        }
+
+        var response, settings = {
+            async: false,
+            cache: false,
+            type: method,
+            'data': data || {},
+            'url': base + url,
+            success: function (data) {
+                response = data;
+            }
+        };
+        settings.data.ns = pub.ns;
+
+        // this really shouldn't be synchronous, but we'd have to
+        // change the public API otherwise :/
+        $.ajax(settings);
+
+        // assume success was executed
+        return response;
     }
 
-    pub.key = function(index) {
-	return 'keytext';
-    }
+    // not part of http://dev.w3.org/html5/webstorage/#storage
+    // however, it might be useful to be able to change your namespace
+    pub.ns = namespace;
 
-    pub.getItem = function(key) {
-	return 'valuetext';
-    }
+    Object.defineProperty(pub, 'length', {get: function () {
+        return makeRequest('GET', '/length');
+    }});
 
-    pub.setItem = function(key, value) {
-    }
+    pub.clear = function () {
+        makeRequest('DELETE');
+    };
 
-    pub.removeItem = function(key) {
-    }
+    pub.key = function (index) {
+        return makeRequest('GET', '/key/' + index.toString());
+    };
+
+    pub.getItem = function (key) {
+        return makeRequest('GET', {'key': key});
+    };
+
+    pub.setItem = function (key, value) {
+        // we use URL to pass a query string because... maybe the API
+        // should be better?
+        return makeRequest('POST', '?key=' + key, {'value': value});
+    };
+
+    pub.removeItem = function (key) {
+        makeRequest('DELETE', {'key': key});
+    };
 
     return pub;
-})(jQuery);
+}(window, jQuery));
